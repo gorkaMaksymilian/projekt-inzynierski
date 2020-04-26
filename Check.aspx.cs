29e9257e09bb07f10f1cc6bf8a84240268d27350
MichalGregorczyk <45073501+MichalGregorczyk@users.noreply.github.com>
2020-04-26 19:26:31 +0200

    Add files via upload

---
+++ Check.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Glosowaniev3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        static string connectionString = "Data Source=MICHAŁ\\SQLEXPRESS;Initial Catalog=Glosowanie;Integrated Security=True";
+        SqlConnection conn = new SqlConnection(connectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TypePass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonPass_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter;
+            DataSet ds = new DataSet();
+
+            String query = "select UsrAnswers.answr_1,UsrAnswers.answr_2,UsrAnswers.answr_3 from UsrAnswers where passwd = '" + TypePass.Text + "';";
+            SqlCommand command = new SqlCommand(query,conn);
+            //command.CommandText = "select UsrAnswers.answr_1,UsrAnswers.answr_2,UsrAnswers.answr_3 from UsrAnswers where passwd = '"+TypePass.Text+"';";
+            // conn.Open();
+            adapter = new SqlDataAdapter(query, conn);
+            adapter.Fill(ds);
+
+     
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+            conn.Close(); 
+
+        }
+    }
+}
